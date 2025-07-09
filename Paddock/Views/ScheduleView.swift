0a5eb2698bc340ccab11a8cb60c7d6accee93a2b
Francisco Javier Cortez <francisco.cortez01@utrgv.edu>
2025-07-09 15:49:59 -0500

    Finished tab bar with only animation on tapping not on swiping

--- Paddock/Views/ScheduleView.swift
+++ Paddock/Views/ScheduleView.swift
@@ -8,12 +8,40 @@
 import SwiftUI
 
 struct ScheduleView: View {
-    // Tab Bar View properties
     @State private var activeTab1: TabBar = .previous
-    
+
     var body: some View {
         VStack(spacing: 20) {
-            // Tab Bar
+            Tabbar(.gray)
+                .overlay {
+                    GeometryReader { geo in
+                        let width = geo.size.width
+                        let tabCount = CGFloat(TabBar.allCases.count)
+                        let capsuleWidth = width / tabCount
+                        let index = CGFloat(TabBar.allCases.firstIndex(of: activeTab1) ?? 0)
+                        
+                        ZStack(alignment: .leading) {
+                            Capsule()
+                                .fill(.black)
+                                .frame(width: capsuleWidth)
+                                .offset(x: index * capsuleWidth)
+
+                            Tabbar(.white, .semibold)
+                                .mask(alignment: .leading) {
+                                    Capsule()
+                                        .frame(width: capsuleWidth)
+                                        .offset(x: index * capsuleWidth)
+                                }
+                        }
+                    }
+                }
+                .background(.ultraThinMaterial)
+                .clipShape(.capsule)
+                .shadow(color: .black.opacity(0.1), radius: 5, x: 5, y: 5)
+                .shadow(color: .black.opacity(0.05), radius: 5, x: -5, y: -5)
+                .padding([.horizontal, .top], 15)
+
+            // Tab View
             TabView(selection: $activeTab1) {
                 ForEach(TabBar.allCases, id: \.self) { tab in
                     tab.view
@@ -21,6 +49,26 @@ struct ScheduleView: View {
                 }
             }
             .tabViewStyle(.page(indexDisplayMode: .never))
+        }
+    }
+
+    @ViewBuilder
+    func Tabbar(_ tint: Color, _ weight: Font.Weight = .regular) -> some View {
+        HStack(spacing: 0) {
+            ForEach(TabBar.allCases, id: \.rawValue) { tab in
+                Text(tab.rawValue)
+                    .font(.callout)
+                    .fontWeight(weight)
+                    .foregroundStyle(tint)
+                    .padding(.vertical, 10)
+                    .frame(maxWidth: .infinity)
+                    .contentShape(.rect)
+                    .onTapGesture {
+                        withAnimation(.snappy(duration: 0.3, extraBounce: 0)) {
+                            activeTab1 = tab
+                        }
+                    }
+            }
         }
     }
 }
@@ -35,21 +83,42 @@ class PageOffsetObserver: NSObject {
     var offset: CGFloat = 0
     private(set) var isObserving: Bool = false
     
+    deinit {
+        remove()
+    }
+    
     func observe() {
-        
+        if ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1" {
+            return
+        }
+
+        guard let collectionView, !isObserving else { return }
+        collectionView.addObserver(self, forKeyPath: "contentOffset", context: nil)
+        isObserving = true
     }
     
     func remove() {
-        
+        isObserving = false
+        collectionView?.removeObserver(self, forKeyPath: "contentOffset")
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "contentOffset",
+           let collectionView = object as? UICollectionView {
+            offset = collectionView.contentOffset.x
+        }
     }
 }
 
 struct FindCollectionView: UIViewRepresentable {
+    var result: (UICollectionView) -> ()
     func makeUIView(context: Context) -> UIView {
         let view = UIView()
         view.backgroundColor = .clear
         DispatchQueue.main.asyncAfter(deadline: .now()) {
-            
+            if let collectionView = view.collectionSuperView {
+                result(collectionView)
+            }
         }
         
         return view
@@ -57,5 +126,16 @@ struct FindCollectionView: UIViewRepresentable {
     
     func updateUIView(_ uiView: UIView, context: Context) {
         
+    }
+}
+
+extension UIView {
+    // Finding the CollectionView by traversing the superview
+    var collectionSuperView: UICollectionView? {
+        if let collectionView = superview as? UICollectionView {
+            return collectionView
+        }
+        
+        return superview?.collectionSuperView
     }
 }
